5a50613042d247ef77dd888aa753bae8c245087b
r34p3r <danieljmoore123@gmail.com>
2019-07-13 20:42:03 -0700

    recommit

--- test.py
+++ test.py
@@ -6,8 +6,10 @@ commit_message = input('Enter commit message: ')
 init = 'git init'
 add = 'git add .'
 commit = 'git commit -m %s' % (commit_message)
-remote_add = 'git remote add origin https://github.com/dmoore90/%s.git' % (commit_message)
+remote_add = 'git remote add origin https://github.com/dmoore90/%s.git' % (file_name)
 push = 'git push -u origin master'
+
+
 
 os.system(init)
 os.system(add)
