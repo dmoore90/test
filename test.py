6dba78472f95659d6b212765c08eb094e89b1628
r34p3r <danieljmoore123@gmail.com>
2019-07-13 20:53:23 -0700

    test

--- test.py
+++ test.py
@@ -9,11 +9,13 @@ commit = 'git commit -m %s' % (commit_message)
 remote_add = 'git remote add origin https://github.com/dmoore90/%s.git' % (file_name)
 push = 'git push -u origin master'
 
-
-
 os.system(init)
 os.system(add)
 os.system(commit)
 os.system(remote_add)
 os.system(push)
 
+#l = (init, add, commit, remote_add, push)
+
+#for i in l:
+#	os.system(str(i))
