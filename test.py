6d34911dd6fb82528ad6b49f3e2c7919b0981bf7
r34p3r <danieljmoore123@gmail.com>
2019-07-13 20:38:41 -0700

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+
+file_name = input('Enter file name: ')
+commit_message = input('Enter commit message: ')
+
+init = 'git init'
+add = 'git add .'
+commit = 'git commit -m %s' % (commit_message)
+remote_add = 'git remote add origin https://github.com/dmoore90/%s.git' % (commit_message)
+push = 'git push -u origin master'
+
+os.system(init)
+os.system(add)
+os.system(commit)
+os.system(remote_add)
+os.system(push)
+
